df29559b2382f51cee59312a1a9021ab8794842b
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-25 22:06:32 +0200

    some changes creation appheader component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   data() {
     return {
       title: "Hello ",
+      projects: [],
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  // la sintassi del component cosi' e' condensata quella completa sarebbe cosi'
+  // components:{AppHeader:AppHeader},
+  // ossia {nome che il componente che avra' nel template:parte che punta alla import}
+  // chiave e variabile con stesso nome si possono condensare
+
+  components: {
+    AppHeader,
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+  // ricordarci del this per accedere ai metodi e a data
+
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
 <template>
+  <AppHeader />
+
   <h1>{{ title }}</h1>
+  <ul>
+    <li v-for="project in projects">
+      {{ project.name }}
+    </li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+// non vogliamo i link perche le app di vue sono app single page che non si devono ricaricare
+
+export default {
+  name: "AppHeader",
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Front Project Portfolio</span>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
