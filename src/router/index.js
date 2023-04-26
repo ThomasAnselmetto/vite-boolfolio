df80f20f35e0bf3e16741672579f6d14d2f8f642
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-26 15:55:28 +0200

    install vue-router cr file js  and import

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// creato il file js per la gestione del router importiamo createRouter
+
+import {createRouter,createWebHistory} from 'vue-router';
+
+
+// todo:import pages
+
+
+//todo: creazione routes
+
+
+//todo:export router
